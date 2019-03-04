d9d0818dafd2f5f4102f9133ccdabe2bc7ee892e
unknown <santhosh274@gmail.com>
2019-03-04 22:10:07 +0530

    created components for zoom charts

---
+++ zoomchart-angular-integration/src/app/zoom-charts/network-charts/network-charts.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-network-charts',
+  templateUrl: './network-charts.component.html',
+  styleUrls: ['./network-charts.component.css']
+})
+export class NetworkChartsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
